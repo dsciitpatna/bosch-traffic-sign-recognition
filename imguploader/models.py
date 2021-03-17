88b958e4f71c9d1d4c964250d9cb8372a32047c2
atm1504 <atm1504.in@gmail.com>
2021-03-17 21:36:49 +0530

    Fix image upload

--- imguploader/models.py
+++ imguploader/models.py
@@ -1,5 +1,26 @@
 from django.db import models
+import random
+import os
 
-# Create your models here.
+
+def get_filename_ext(filepath):
+    base_name = os.path.basename(filepath)
+    name, ext = os.path.splitext(base_name)
+    return name, ext
+
+
+def upload_image_path(instance, filename):
+    print(instance)
+    new_filename = random.randint(1, 3910209312)
+    name, ext = get_filename_ext(filename)
+    final_filename = '{new_filename}{ext}'.format(
+        new_filename=new_filename, ext=ext)
+    return "products/{new_filename}/{final_filename}".format(
+        new_filename=new_filename,
+        final_filename=final_filename
+    )
+
+
 class ImgUp(models.Model):
-    image = models.ImageField(blank=True)
+    image = models.ImageField(
+        upload_to=upload_image_path, null=True, blank=True)
--- imguploader/urls.py
+++ imguploader/urls.py
@@ -1,12 +1,13 @@
-from django.urls import re_path
+from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 app_name = 'imguploader'
 
 urlpatterns = [
-    re_path(r'^$',views.image_upload,name="imageup"),
+    path('',views.image_upload,name="imageup"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
