0ced0c900ded7c5d388facf12357cf007905132c
atm1504 <atm1504.in@gmail.com>
2021-03-18 01:53:46 +0530

    refactoring

--- BoschTrafficRecognition/settings.py
+++ BoschTrafficRecognition/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'csv_files.apps.CsvFilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'imguploader',
-    'csv_files'
+
 ]
 
 MIDDLEWARE = [
--- csv_files/urls.py
+++ csv_files/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .  import views
+from .views import profile_upload, export_users_csv, show
 
 urlpatterns = [
-    path('upload-csv/',views.profile_upload, name='profile_upload'),
-    path('export/', views.export_users_csv, name='export_users_csv'),
+    path('upload-csv/',profile_upload, name='profile_upload'),
+    path('export/', export_users_csv, name='export_users_csv'),
+    path('show/', show, name ="show")
     
 ]
