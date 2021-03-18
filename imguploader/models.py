2aeeb64299c475c28a29c03f7a044686ed34dc9e
atm1504 <atm1504.in@gmail.com>
2021-03-18 14:52:55 +0530

    Slug initial setup

--- imguploader/models.py
+++ imguploader/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 import random
 import os
+from django.db.models.signals import pre_save, post_save
 
+from .utils import unique_slug_generator
 
 def get_filename_ext(filepath):
     base_name = os.path.basename(filepath)
@@ -22,5 +24,19 @@ def upload_image_path(instance, filename):
 
 
 class ImgUp(models.Model):
+    # Slug for trversing between images usign custom url
+    slug = models.SlugField(blank=True, unique=True)
     image = models.ImageField(
         upload_to=upload_image_path, null=True, blank=True)
+
+    
+
+
+# its is a function thats is called by the pre_sav funtion automatically
+# when a new image is uploaded
+def product_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+# It gets executed automatically whenever new image is uploaded.
+pre_save.connect(product_pre_save_receiver, sender=ImgUp)
