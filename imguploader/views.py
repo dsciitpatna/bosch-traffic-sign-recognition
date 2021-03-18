e10db83dca3f14bf8c93cd8e93e71191913bedf9
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-03-19 00:58:29 +0530

    Added checks if no file selected(ImgUploader)

--- imguploader/views.py
+++ imguploader/views.py
@@ -5,17 +5,24 @@ from django.http import Http404
 
 from .models import ImgUp
 
+import logging
+logger = logging.getLogger(__name__)
+
 # Create your views here.
 
 
 def image_upload(request):
     if request.method == 'POST':
         form = forms.UploadImage(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return render(request, 'imguploader/imgup.html', {'result':'success'})
+        try:
+            img_file = request.FILES['image']
+            if (img_file.name.endswith('.jpg')) or (img_file.name.endswith('.jpeg')) or (img_file.name.endswith('.png')):
+                form.save()
+                return render(request, 'imguploader/imgup.html', {'result':'success'})
+        except: 
+            return render(request,'imguploader/imgup.html',{'error':'No File selected'})    
     else:
-        form = forms.UploadImage()
+       form = forms.UploadImage()
     return render(request, 'imguploader/imgup.html', {'form': form})
 
 
